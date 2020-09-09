4e8355d009b4b9685510d5341513092d9728a6b0
Jose_Martinez <martinez022jose@gmail.com>
2020-09-09 16:41:41 -0300

    Se agrega nav

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const app = express();
+const ex = express();
 const path = require('path');
 
 //Settings
-app.set('port', 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('views engine', 'ejs');
+ex.set('port', 3000);
+ex.set('views', path.join(__dirname, 'views'));
+ex.engine('html', require('ejs').renderFile);
+ex.set('views engine', 'ejs');
 
 //Routes
-app.use(require('./routes/routes.js'));
+ex.use(require('./routes/routes.js'));
 
 //listening the server
-app.listen(app.get('port'), () => {
-    console.log('Se esta utilizando server port: ', app.get('port'));
+ex.listen(ex.get('port'), () => {
+    console.log('Se esta utilizando server port: ', ex.get('port'));
 });
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index.ejs',{ title :'Pagina principal'});
+    res.render('index.html',{ title :'Pagina principal'});
 });
 
 router.get('/contact', (req, res) => {
-    res.render('contact.ejs',{ title :'Pagina secundaria'});
+    res.render('contact.html',{ title :'Pagina secundaria'});
 });
 
 module.exports = router;
