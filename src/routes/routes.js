d07e6c3cc3c1645ac74933a990688f540316a840
Jose_Martinez <martinez022jose@gmail.com>
2020-09-12 18:14:19 -0300

    Se agrega animacion sobre nav

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index.html',{ title : 'index'});
+    res.render('index.html',{ title : 'Home'});
 });
 
 router.get('/contact', (req, res) => {
@@ -10,11 +10,11 @@ router.get('/contact', (req, res) => {
 });
 
 router.get('/aboutUs',(req,res) => {
-    res.render('aboutUs.html',{ title : 'aboutUs'});
+    res.render('aboutUs.html',{ title : 'AboutUs'});
 });
 
 router.get('/opportunities',(req, res) => {
-    res.render('opportunities.html', {title : 'opportunities'});
+    res.render('opportunities.html', {title : 'Opportunities'});
 });
 
 module.exports = router;
