e63e759134c0caf601684c62da4bd942e9ce50c0
Jose_Martinez <martinez022jose@gmail.com>
2020-09-16 18:09:15 -0300

    Se modifica page opportunities

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
+//Install modules
 const express = require('express');
 const ex = express();
 const path = require('path');
 
 //Settings
-ex.set('port', 5000);
-ex.set('views', path.join(__dirname, 'views'));
-ex.engine('html', require('ejs').renderFile);
-ex.set('views engine', 'ejs');
+ex.set('port', 5000); //Especificamos uso de port
+ex.set('views', path.join(__dirname, 'views')); // Especificamos ruta de views
+ex.engine('html', require('ejs').renderFile); //Modificamos uso de extension html
+ex.set('view engine', 'ejs'); //Especificamos uso de motor de plantillas ejs
 
 //Routes
 ex.use(require('./routes/routes.js'));
 
-//Create folder -g 
+//Create folder -g / Se entrega al navegador, para mejorar el frontend
 ex.use(express.static(path.join(__dirname, 'public')));
 
 //listening the server
