5d051f2657fef69cb255c77d4af5827b914af605
Jose_Martinez <martinez022jose@gmail.com>
2020-10-11 11:22:02 -0300

    Se modifica rutas

--- index.js
+++ index.js
@@ -5,15 +5,15 @@ const path = require('path');
 
 //Settings
 ex.set('port', 5000 || process.env.PORT); //Especificamos uso de port
-ex.set('views', path.join(__dirname, 'views')); // Especificamos ruta de views
+ex.set('views', path.join(__dirname, 'src/views')); // Especificamos ruta de views
 ex.engine('html', require('ejs').renderFile); //Modificamos uso de extension html
 ex.set('view engine', 'ejs'); //Especificamos uso de motor de plantillas ejs
 
 //Routes
-ex.use(require('./routes/routes.js'));
+ex.use(require('./src/routes/routes.js'));
 
 //Create folder -g / Se entrega al navegador, para mejorar el frontend
-ex.use(express.static(path.join(__dirname, 'public')));
+ex.use(express.static(path.join(__dirname, 'src/public')));
 
 //listening the server
 ex.listen(ex.get('port'), () => {
