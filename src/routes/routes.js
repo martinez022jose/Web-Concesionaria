8839a20999becb2072c9dad4955ad9e4268529d7
Jose_Martinez <martinez022jose@gmail.com>
2020-10-15 13:43:28 -0300

    heroku v5

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const nodemailer = require('nodemailer');
 
 router.get('/', (req, res) => {
     res.render('../views/index.html',{ title : 'Home'});
@@ -15,6 +16,36 @@ router.get('/aboutUs',(req,res) => {
 
 router.get('/opportunities',(req, res) => {
     res.render('../views/opportunities.html', {title : 'Opportunities'});
+});
+
+router.post('/sendMail', async (req, res) => {
+    const nombre = req.body.nombre;
+    const email = req.body.email;
+    const mensaje = req.body.mensaje;
+
+    const transporter = nodemailer.createTransport('SMTP', {
+        service: 'gmail', //al usar un servicio bien conocido, no es necesario proveer un nombre de servidor.
+        auth: {
+          user: 'martinez022jose@gmail.com',
+          pass: 'mataderos22'
+        },
+      });
+      
+    const mailOptions = {
+        from: 'jose',
+        to: 'unmail',
+        subject: 'chiques',
+        text: 'hola todes'
+    }
+    transporter.sendMail(mailOptions, (error,respuesta) => {
+        if(error){
+            console.log(error);
+        }else{
+            res.redirect('/');
+        }
+    });
+
+    
 });
 
 module.exports = router;
