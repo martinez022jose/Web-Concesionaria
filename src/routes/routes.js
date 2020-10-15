bb9dc86db53633cd260e786c3074bbc9be8e8b0c
Jose_Martinez <martinez022jose@gmail.com>
2020-10-14 21:45:06 -0300

    v3

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,19 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index.html',{ title : 'Home'});
+    res.render('../views/index.html',{ title : 'Home'});
 });
 
 router.get('/contact', (req, res) => {
-    res.render('contact.html',{ title : 'Contact'});
+    res.render('../views/contact.html',{ title : 'Contact'});
 });
 
 router.get('/aboutUs',(req,res) => {
-    res.render('aboutUs.html',{ title : 'AboutUs'});
+    res.render('../views/aboutUs.html',{ title : 'AboutUs'});
 });
 
 router.get('/opportunities',(req, res) => {
-    res.render('opportunities.html', {title : 'Opportunities'});
+    res.render('../views/opportunities.html', {title : 'Opportunities'});
 });
 
 module.exports = router;
