1fa04669e53e5cd3c8fd6980953730174b618f32
Jose_Martinez <martinez022jose@gmail.com>
2020-10-17 00:04:06 -0300

    Se agrega sistema de mail + responsive design de section aboutUss

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,10 @@ const router = express.Router();
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const smtpTransport = require('nodemailer-smtp-transport');
+const xoauth2 = require('xoauth2');
+//const smtpPool = require('nodemailer-smtp-pool');
+
 
 router.get('/', (req, res) => {
     res.render('../views/index.html',{ title : 'Home'});
@@ -26,20 +30,32 @@ router.post('/send-email', urlencodedParser, async (req, res) => {
     const email = req.body.email;
     const texto = req.body.texto;
 
-   
-    var transporter = nodemailer.createTransport("SMTP", {
-        service: "Gmail", //al usar un servicio bien conocido, no es necesario proveer un nombre de servidor.
+   //"SMTP",smtpTransport( 
+    var transporter = nodemailer.createTransport(smtpTransport({
+        service: 'Gmail',
         auth: {
-          user: "martinez022jose@gmail.com",
-          pass: "mataderos32"
-        },
-      });
+            user: 'martinez022jose@gmail.com',
+            pass: "mataderos32",
+            //xoauth2: xoauth2.createXOAuth2Generator({
+                //type: 'xoauth2',
+                //type: 'OAuth2',
+                
+                //clientId: '228993061492-u4hkahcfau09rp0c15kt62gvqb33tthk.apps.googleusercontent.com',
+                //clientSecret: 'p9DDjHaQRIiowFLgYLzay_o2',
+                //accessToken: 'ya29.a0AfH6SMCWGbsJSsgLZhJSu3z4ofGBQ25kErppmisASlCqnJvW5uIeGDcmCnuTXf40D5QjLhSXMFmwpRsjY1aDp_iAKUOBqegVBYfMzg5SBB2j1bavg7lr7-3cTCHQ03HbXb7XV19EcOfRs6MLopgXy6JjdANDiWgCmnc',
+                //refreshToken: '1//04_gjUi68L0J4CgYIARAAGAQSNwF-L9IrQNOXGQmLq0--3Ch9K1tp7VDHpwgbU2zqJyfA4raN9O0q2J1Fs4qEHuEAf7-db1O-rYQ',
+                
+                
+       
+           // })
+        }
+    }));
       
     var mailOptions = {
         from: "martinez022jose <martinez022jose@gmail.com>",
-        to: req.body.email,
-        subject: "chiques",
-        text: "hola todes"
+        to: email,
+        subject: "Web concesionaria",
+        text: `mensaje: ${texto} ` 
     }
 
     await transporter.sendMail(mailOptions, (error,respuesta) => {
